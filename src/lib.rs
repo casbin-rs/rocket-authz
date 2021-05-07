ed52030261b98d747ab91048c9747e275200a7e3
RobotHuang <1183598761@qq.com>
2021-05-07 17:28:05 +0800

    feat: Implement CasbinFairing
    Signed-off-by: RobotHuang <1183598761@qq.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,28 @@
 use rocket::{
     fairing::{Info, Kind, Fairing},
+    request::{self, FromRequest, Request},
+    Data,
+    http::{Status},
 };
 
 use std::sync::{Arc, RwLock};
 use casbin::prelude::*;
 use casbin::{CachedEnforcer, CoreApi, Result as CasbinResult};
+
+#[derive(Clone)]
+pub struct CasbinGuard(Option<Status>);
+
+impl<'a, 'r> FromRequest<'a, 'r> for CasbinGuard {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<CasbinGuard, ()> {
+        match *request.local_cache(|| CasbinGuard(Status::from_code(0))) {
+            CasbinGuard(Some(Status::Ok)) => request::Outcome::Success(CasbinGuard(Some(Status::Ok))),
+            CasbinGuard(Some(err_status)) => request::Outcome::Failure((err_status, ())),
+            _ => request::Outcome::Failure((Status::BadGateway, ())),
+        }
+    }
+}
 
 #[derive(Clone)]
 pub struct CasbinFairing {
@@ -34,6 +52,56 @@ impl Fairing for CasbinFairing {
         Info {
             name: "Casbin Fairing",
             kind: Kind::Request | Kind::Response,
+        }
+    }
+
+    fn on_request(&self, request: &mut Request, _data: &Data) {
+        let cloned_enforce = self.enforcer.clone();
+        let path = request.uri().path().to_owned();
+        let action = request.method().as_str().to_owned();
+        // Get subject and domain from cookie.
+        let subject = request.cookies().get("subject").map(|x| x.to_owned());
+        let domain = request.cookies().get("domain").map(|x| x.to_owned());
+
+        if let Some(subject) = subject {
+            if let Some(domain) = domain {
+                let subject_str = subject.value().to_string();
+                let domain_str = domain.value().to_string();
+                let mut lock = cloned_enforce.write().unwrap();
+                match lock.enforce_mut(vec![subject_str, domain_str, path, action]) {
+                    Ok(true) => {
+                        drop(lock);
+                        request.local_cache(||{CasbinGuard(Some(Status::Ok))});
+                    }
+                    Ok(false) => {
+                        drop(lock);
+                        request.local_cache(|| CasbinGuard(Some(Status::Forbidden)));
+                    }
+                    Err(_) => {
+                        drop(lock);
+                        request.local_cache(|| CasbinGuard(Some(Status::BadGateway)));
+                    }
+                };
+            } else {
+                let subject_str = subject.value().to_string();
+                let mut lock = cloned_enforce.write().unwrap();
+                match lock.enforce_mut(vec![subject_str, path, action]) {
+                    Ok(true) => {
+                        drop(lock);
+                        request.local_cache(||{CasbinGuard(Some(Status::Ok))});
+                    }
+                    Ok(false) => {
+                        drop(lock);
+                        request.local_cache(|| CasbinGuard(Some(Status::Forbidden)));
+                    }
+                    Err(_) => {
+                        drop(lock);
+                        request.local_cache(|| CasbinGuard(Some(Status::BadGateway)));
+                    }
+                };
+            }
+        } else {
+            request.local_cache(|| CasbinGuard(Some(Status::BadGateway)));
         }
     }
 }
